6aba27e4b7f8d4373452e2c4a95eb1a676a1535b
netizen-cloud <talhazubairmx@gmail.com>
2025-03-18 19:30:06 +0500

    This is the basic commit for Rabbit MQ.

---
+++ Receiver/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqp://guest:guest@localhost:5674");
+
+connectionFactory.ClientProvidedName = "Receiver RabbitMQ";
+
+using var connection =await connectionFactory.CreateConnectionAsync();
+using var channel =await connection.CreateChannelAsync();
+
+
+string _exchangeName = "Demo Exchange";
+string _queueName = "Demo Queue";
+string _routingKey = "Demo Queue";
+
+await channel.ExchangeDeclareAsync(_exchangeName, ExchangeType.Direct);
+await channel.QueueDeclareAsync(_queueName, false, false, false, null);
+await channel.QueueBindAsync(_queueName, _exchangeName, _routingKey, null);
+
+await channel.BasicQosAsync(0, 1 , false);
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, args) =>
+{
+   // Console.WriteLine($"Inside msg received block");
+    Thread.Sleep(TimeSpan.FromSeconds(3));
+    var body = args.Body.ToArray();
+    String msg = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {msg}");
+    await channel.BasicAckAsync(args.DeliveryTag, false);
+};
+
+
+
+var cosumerTag = channel.BasicConsumeAsync(_queueName , false , consumer);
+
+Console.ReadLine();
+
+await channel.CloseAsync();
+
+await connection.CloseAsync();
---
+++ Sender/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqp://guest:guest@localhost:5674");
+
+connectionFactory.ClientProvidedName = "Sender RabbitMQ";
+
+using var connection =await connectionFactory.CreateConnectionAsync();
+using var channel =await connection.CreateChannelAsync();
+
+
+string _exchangeName = "Demo Exchange";
+string _queueName = "Demo Queue";
+string _routingKey = "Demo Queue";
+
+await channel.ExchangeDeclareAsync(_exchangeName, ExchangeType.Direct);
+await channel.QueueDeclareAsync(_queueName, false, false, false, null);
+await channel.QueueBindAsync(_queueName, _exchangeName, _routingKey, null);
+
+
+BasicProperties properties = new BasicProperties();
+
+Console.WriteLine("Type any message to send to Rabbit MQ");
+string input = string.Empty;
+while (input != "exit")
+{
+    input = Console.ReadLine();
+    
+    byte[] msgBody = Encoding.UTF8.GetBytes($"{input} : {DateTime.Now}");
+    
+    await channel.BasicPublishAsync(_exchangeName, _routingKey, mandatory:true, basicProperties:properties, msgBody );
+    
+    Console.WriteLine($"[x] {input} [Sent], \n To send another one to Rabbit MQ or Type \"exit\" to exit");
+}
+
+await channel.CloseAsync();
+
+await connection.CloseAsync();
+
+
+
+
+
+
+
